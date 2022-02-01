07f5f1aefb091a30fd4756c8f135715dd1eb8f1e
PIERRE Guillaume <proguillaume.pierre@gmail.com>
2022-02-01 14:10:14 +0100

    hey

--- partie_guigui.ml
+++ partie_guigui.ml
@@ -53,52 +53,30 @@ let is_puiss_2(n : int) : bool =
        
     ;;
 
-   let(i, pic) = random_img 10 50;;
+   let(i, pic) = random_img 16 30;;
    open_graph "";;
    draw_picture pic;;
    close_graph();;
 
 
-
-   let a : arbre = Noeud(Feuille(Noir), Feuille(Blanc), Feuille(Noir), Feuille(Blanc));;
-   
-
-
-
-
 let image_vers_arbre k img =
   let rec iva_aux mi mxi my mxy img =
-    let m = mxi-mi/2 in
+    let halfi = (mxi+mi)/2 in
+    let halfiy = (mxy+my)/2 in
     if mxi - mi = 1 ||  mi - mxi = 1
     then Noeud(Feuille(img.(mi).(my)), Feuille(img.(mi).(mxi)), Feuille(img.(mxi).(mi)), Feuille(img.(mxi).(mxy)))
     else
-         Noeud(iva_aux mi m my m img, 
-               iva_aux mi m (m+1) mxy img,
-               iva_aux (m+1) mxi my m img,
-               iva_aux (m+1) mxi (m+1) mxy img
-    )
+      Noeud(iva_aux mi halfi my halfiy img, 
+      iva_aux mi halfi (halfiy+1) mxy img,
+      iva_aux (halfi+1) mxi my halfiy img,
+      iva_aux (halfi+1) mxi (halfiy+1) mxy img
+  )
     in iva_aux 0 (k-1) 0 (k-1) img
 ;;
 
-let image_vers_arbre k img =
-  let rec iva_aux mi mxi my mxy img =
-    let m = mxi-mi/2 in
-    if mxi - mi = 1
-    then Noeud(Feuille(Noir), Feuille(Noir), Feuille(Noir), Feuille(Noir))
-    else
-         Noeud(iva_aux mi m my m img, 
-               iva_aux mi m (m+1) mxy img,
-               iva_aux (m+1) mxi my m img,
-               iva_aux (m+1) mxi (m+1) mxy img
-    )
-    in iva_aux 0 (k-1) 0 (k-1) img
-;;
 
-#trace image_vers_arbre;;
-image_vers_arbre 4  pic;;
+image_vers_arbre 16 pic;;
 
-
-3/2;;
 
 
 let remplir_img img k i j c =
@@ -118,6 +96,8 @@ let remplir_img img k i j c =
     );
   )      
 ;;
+
+
 
 let (a, p) = remplir_img pic 4 4 0 Blanc;;
 p.(1).(1);;
